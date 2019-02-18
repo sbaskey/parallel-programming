7106f1d5db6937ddc8b7de982295bedc64b7517a
sbaskey <43499610+sbaskey@users.noreply.github.com>
2019-02-18 17:50:03 +0530

    comparing running time for mutex and busy wait

---
+++ mutex vs busy_wait/series_mutex.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+pthread_mutex_t lock;
+double sum=0;
+long long iters=0;
+int No_threads;
+int flag=0;
+void *compute_sum(void *rank);
+int main()
+{
+    pthread_t thread[100];
+    double val=0.0;
+    long i;
+    printf("enter the value of iterater\n");
+    scanf("%lld",&iters);
+    printf("enter the number of threads\n");
+    scanf("%d",&No_threads);
+
+    clock_t t; 
+    t = clock(); 
+
+    for(  i=0; i<No_threads; i++)
+      pthread_create(&thread[i], NULL,compute_sum,(void*)i); 
+    for(  i= 0; i<No_threads; i++) 
+    pthread_join(thread[i], NULL);
+    val =sum;
+
+  t = clock() - t; 
+  double time_taken = ((double)t)/CLOCKS_PER_SEC;
+  printf("time taken for execution=%f \n", time_taken); 
+  printf("iters: %lld val: %.2lf\n", iters, val);
+  return 0;
+}
+void *compute_sum(void *rank) 
+{
+  long my_rank = (long) rank;
+  printf("%ld",my_rank);
+  double factor;
+  long long i;
+  long long my_n = iters/No_threads;
+  long long my_first_i = my_n * my_rank;
+  long long my_last_i = my_first_i + my_n;
+  long long my_sum;
+
+
+for (i = my_first_i+1; i <=my_last_i; i++) 
+    my_sum +=i;
+
+pthread_mutex_lock(&lock);
+sum += my_sum;
+pthread_mutex_unlock(&lock);				
+  return NULL;
+}
